816501567fbd74d342114cc7ec60573f4aa078fd
Carlos Cardona <cgcardona@gmail.com>
2013-05-28 18:22:52 -0700

    Move Mirror API CRUD methods to ApplicationController so that all controllers get them via inheritance.

--- app/controllers/example_controller.rb
+++ app/controllers/example_controller.rb
@@ -9,35 +9,4 @@ class ExampleController < ApplicationController
 
     render :index
   end
-
-  def add_card
-    client = User.get_client(current_user)
-    mirror = client.discovered_api('mirror', 'v1')
-    timeline_item = mirror.timeline.insert.request_schema.new({ 'text' => "Hello World!" })
-
-    result = client.execute(
-      :api_method => mirror.timeline.insert,
-      :body_object => timeline_item)
-
-    if result.status == 200
-      @message = "Inserted new card with ID #{result.data.id}"
-    else
-      @message = "An error occurred: #{result.data['error']['message']}"
-    end
-    render :add_card
-  end
-
-  def get_location
-    client = User.get_client(current_user)
-    mirror = client.discovered_api('mirror', 'v1')
-    result = client.execute(
-      :api_method => mirror.locations.get,
-      :parameters => { 'id' => 'latest' })
-    if result.status == 200
-      location = result.data
-      @message = "Location recorded on: #{location.timestamp}<br />Lat: #{location.latitude}<br />Long: #{location.longitude}<br />Accuracy: #{location.accuracy} meters"
-    else
-      @message = "An error occurred: #{result.data['error']['message']}"
-    end
-  end
 end
